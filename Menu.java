891139be582717c11a66f1e19b1a7c3058142a0f
Kaellen-heleni <KAELLENHELENI@ICLOUD.COM>
2023-11-21 20:49:10 -0300

    Adicionado as Classes de Conta Corrente e Poupança ao Menu e realizado testes

--- Menu.java
+++ Menu.java
@@ -3,17 +3,35 @@ package conta;
 import java.util.Scanner;
 import conta.util.Cores;
 import conta.model.Conta;
+import conta.model.ContaCorrente;
+import conta.model.ContaPoupanca;
 
 public class Menu {
 
 	public static void main(String[] args) {
-		
+		// Teste da Classe Conta
 		Conta c1 = new Conta(1, 123, 1, "Adriana",  1000.0f);
 		c1.visualizar();
 		c1.sacar(12000.0f);
 		c1.visualizar();
 		c1.depositar(5000.0f);
 		c1.visualizar();
+		
+		// Teste da classe conta corrente
+		ContaCorrente cc1 = new ContaCorrente(2, 123, 1,"Kaellen", 15000.0f, 1000.0f);
+		cc1.visualizar();
+		cc1.sacar(12000.0f);
+		cc1.visualizar();
+		cc1.depositar(5000.0f);
+		cc1.visualizar();
+		
+		// Teste da classe conta poupança
+		ContaPoupanca cp1 = new ContaPoupanca(3, 123, 2, "Victor", 100000.0f, 15);
+		cp1.visualizar();
+		cp1.sacar(1000.0f);
+		cp1.visualizar();
+		cp1.depositar(5000.0f);
+		cp1.visualizar();
 		
 		Scanner receba = new Scanner(System.in);
 	
